ce587694f9204d96bfaaa6528e4c53856e892841
kayhide <kayhide@gmail.com>
2018-11-28 01:32:33 +0900

    calculate minmaxes of time of each day.

--- src/Daimust/Histfile.hs
+++ src/Daimust/Histfile.hs
@@ -5,13 +5,16 @@ where
 
 import           ClassyPrelude              hiding (some, (</>))
 
-import           Control.Lens               ((%~), (&), (^.), (^..), _1)
+import           Control.Lens               (to, (^.), (^..), _1)
+import           Data.Semigroup             (Max (..), Min (..))
 import           Data.Time.Clock.POSIX      (posixSecondsToUTCTime)
 import           Data.Time.Lens
-import           Data.Time.LocalTime        (getCurrentTimeZone, utcToLocalTime)
+import           Data.Time.LocalTime        (LocalTime, TimeOfDay,
+                                             getCurrentTimeZone)
 import           Path                       (Abs, File, Path, mkRelFile,
-                                             toFilePath, (</>))
+                                             parseAbsFile, toFilePath, (</>))
 import           Path.IO                    (getHomeDir)
+import           System.Environment         (lookupEnv)
 import           Text.Megaparsec            (Parsec, parseMaybe, some, takeRest)
 import           Text.Megaparsec.Char       (char, digitChar, space)
 import           Text.Megaparsec.Char.Lexer (decimal)
@@ -22,23 +25,30 @@ try = do
   zone <- getCurrentTimeZone
   contents <- readFile . toFilePath =<< histfile
   let lines' = lines $ decodeUtf8 contents
-  let hists = catMaybes $ parse' <$> take 100 (reverse lines')
-  let locals = hists ^.. traverse . _1 . utcInTZ zone
+  let hists = catMaybes $ parse' <$> reverse lines'
+  let timestamps = hists ^.. traverse . _1 . utcInTZ zone :: [LocalTime]
 
-  traverse_ print' $ zip locals $ snd <$> hists
-  -- traverse_ print $ locals ^.. traverse . time
+  traverse_ print . catMaybes $ rangeOfTime <$> groupBy ((==) `on` (^. date)) timestamps
 
   where
     parse' :: Text -> Maybe (UTCTime, Text)
     parse' = parseMaybe histP
 
-    print' (t, command) = putStrLn $ tshow t <> "  " <> command
+    rangeOfTime :: [LocalTime] -> Maybe (Day, (TimeOfDay, TimeOfDay))
+    rangeOfTime [] = Nothing
+    rangeOfTime times@(dt : _) =
+      case times ^.. traverse . time . to (Min &&& Max) of
+        [] -> Nothing
+        (x : xs) -> pure $ (dt ^. date,) $ (getMin *** getMax) $ foldl' (<>) x xs
+
 
 
 histfile :: IO (Path Abs File)
-histfile = do
-  home <- getHomeDir
-  pure $ home </> $(mkRelFile ".histfile")
+histfile = maybe dotHistfile parseAbsFile =<< lookupEnv "HISTFILE"
+  where
+    dotHistfile = do
+      home <- getHomeDir
+      pure $ home </> $(mkRelFile ".histfile")
 
 histP :: Parsec () Text (UTCTime, Text)
 histP = do
