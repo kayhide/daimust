981efeb1191dbac2ff2e1d08dad1f8f75f75eb55
kayhide <kayhide@gmail.com>
2018-11-28 03:05:54 +0900

    update Histfile using Map.

--- src/Daimust/Histfile.hs
+++ src/Daimust/Histfile.hs
@@ -8,8 +8,10 @@ where
 
 import           ClassyPrelude              hiding (some, (</>))
 
-import           Control.Lens               (to, (^.), (^..), _1)
-import           Data.Semigroup             (Max (..), Min (..))
+import           Control.Lens               (to, (^.), (^..), _1, _2)
+import           Data.Map                   (Map)
+import qualified Data.Map                   as Map
+import           Data.Semigroup             (Max (..), Min (..), getMax, getMin)
 import           Data.Time.Clock.POSIX      (posixSecondsToUTCTime)
 import           Data.Time.Lens
 import           Data.Time.LocalTime        (LocalTime, TimeOfDay,
@@ -29,26 +31,27 @@ data HistRecord = HistRecord
   }
   deriving (Eq, Show)
 
+
 readAll :: IO [HistRecord]
 readAll = do
   zone <- getCurrentTimeZone
   contents <- readFile . toFilePath =<< histfile
   let lines' = lines $ decodeUtf8 contents
-  let hists = catMaybes $ parse' <$> reverse lines'
-  let timestamps = hists ^.. traverse . _1 . utcInTZ zone :: [LocalTime]
-  pure $ catMaybes $ toHistRecord <$> groupBy ((==) `on` (^. date)) timestamps
+  let hists = catMaybes $ parseLineMaybe <$> lines'
+  let timestamps = hists ^.. traverse . _1 . utcInTZ zone
+  pure . fmap toHistRecord $ Map.toList $ foldr pushTime Map.empty timestamps
 
-  where
-    parse' :: Text -> Maybe (UTCTime, Text)
-    parse' = parseMaybe histP
 
-    toHistRecord :: [LocalTime] -> Maybe HistRecord
-    toHistRecord [] = Nothing
-    toHistRecord times@(dt : _) =
-      case times ^.. traverse . time . to (Min &&& Max) of
-        [] -> Nothing
-        (x : xs) -> pure $ uncurry (HistRecord (dt ^. date)) $ (getMin *** getMax) $ foldl' (<>) x xs
 
+type DayMinMaxMap = Map Day (Min TimeOfDay, Max TimeOfDay)
+
+toHistRecord :: (Day, (Min TimeOfDay, Max TimeOfDay)) -> HistRecord
+toHistRecord x =
+  HistRecord (x ^. _1) (x ^. _2 . _1 . to getMin) (x ^. _2 . _2 . to getMax)
+
+pushTime :: LocalTime -> DayMinMaxMap -> DayMinMaxMap
+pushTime t =
+  Map.insertWith (<>) (t ^. date) (Min (t ^. time), Max (t ^. time))
 
 
 histfile :: IO (Path Abs File)
@@ -58,8 +61,12 @@ histfile = maybe dotHistfile parseAbsFile =<< lookupEnv "HISTFILE"
       home <- getHomeDir
       pure $ home </> $(mkRelFile ".histfile")
 
-histP :: Parsec () Text (UTCTime, Text)
-histP = do
+
+parseLineMaybe :: Text -> Maybe (UTCTime, Text)
+parseLineMaybe = parseMaybe lineP
+
+lineP :: Parsec () Text (UTCTime, Text)
+lineP = do
   void $ char ':' *> space
   i :: Integer <- decimal
   void $ char ':' *> some digitChar *> char ';'
