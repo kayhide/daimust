c62efb8be9ed9961c286970edeb16eb74cb87767
kayhide <kayhide@gmail.com>
2019-03-04 00:56:01 +0900

    update logger.

--- src/Plugin/Logger/Config.hs
+++ src/Plugin/Logger/Config.hs
@@ -10,6 +10,8 @@ import           Control.Monad.Logger
 
 import           Configurable         (Configurable (..), FetchSetting,
                                        fetchSetting)
+import           System.Console.ANSI
+import           System.Environment   (lookupEnv)
 
 
 data LoggerConfig
@@ -17,12 +19,23 @@ data LoggerConfig
 data LoggerSetting = LoggerSetting
   { _stage   :: !Stage
   , _verbose :: !Bool
+  , _color   :: !Bool
   }
   deriving (Eq, Show)
 
 data Stage = Development | Production | Test
   deriving (Eq, Show, Read)
-  deriving FetchSetting
+
+instance FetchSetting Stage where
+  fetchSetting key def =
+    lookupEnv (unpack key)
+    >>= \case
+      Nothing                         -> pure def
+      Just (toLower -> "production")  -> pure Production
+      Just (toLower -> "test")        -> pure Test
+      Just (toLower -> "development") -> pure Development
+      Just x                          -> fail $ "Unknown Stage: " <> x
+
 
 type LogFunc = LogLevel -> Text -> IO ()
 
@@ -49,16 +62,33 @@ instance Configurable LoggerConfig where
     LoggerSetting
     <$> fetchSetting "LOGGER_STAGE" Development
     <*> fetchSetting "LOGGER_VERBOSE" False
+    <*> fetchSetting "LOGGER_COLOR" True
 
-  start (LoggerSetting _stage _verbose) _ =
+  start (LoggerSetting _stage _verbose _color) _ =
     pure $ LoggerRunning
+    $ bool
     (\level' msg' ->
         runStderrLoggingT $
         filterLogger filter' $
         logOtherN level' msg'
     )
+    (\level' msg' -> do
+        hSetSGR stderr (colorOf level')
+        runStderrLoggingT $
+          filterLogger filter' $
+          logOtherN level' $ msg' <> pack (setSGRCode [Reset])
+    )
+    _color
     where
       filter' = case (_verbose, _stage) of
         (True, _)           -> \_ _ -> True
         (False, Production) -> \_ level'' -> LevelWarn <= level''
         (False, _)          -> \_ level'' -> LevelInfo <= level''
+
+
+colorOf :: LogLevel -> [SGR]
+colorOf LevelDebug = [SetColor Foreground Dull Green]
+colorOf LevelInfo = [SetColor Foreground Dull Blue]
+colorOf LevelWarn = [SetColor Foreground Dull Yellow]
+colorOf LevelError = [SetColor Foreground Dull Red]
+colorOf (LevelOther _) = [SetColor Foreground Dull Magenta]
