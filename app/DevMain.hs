d68209f0ae3c6c2c3a21ee119db4ff68783aaf83
kayhide <kayhide@gmail.com>
2019-04-17 23:20:19 +0900

    update DevMain.

--- app/DevMain.hs
+++ app/DevMain.hs
@@ -16,31 +16,33 @@ run :: IO ()
 run = do
   -- invalidateCache               -- Invalidate cached state immediately.
 
-  -- runCommand ["put", "--help"]
+  -- runCommand "put --help"
 
-  -- runCommand ["put", "12", "1000", "2100"]
-  -- runCommand ["delete", "12"]
-  -- runCommand ["put", "2", "1000", "2100", "--holiday-on"]
-  -- runCommand ["delete", "2"]
-  -- runCommand ["put", "3", "--workday-off"]
-  -- runCommand ["delete", "3"]
+  -- runCommand "put 12 1000 2100"
+  -- runCommand "delete 12"
+  -- runCommand "put 2 1000 2100 --holiday-on"
+  -- runCommand "delete 2"
+  -- runCommand "put 3 --workday-off"
+  -- runCommand "delete 3"
 
-  -- runCommand ["put", "--help"]
-  -- runCommand ["put", "31", "1000", "2100", "--note", "21", "--verbose"]
-  -- runCommand ["delete", "31"]
+  -- runCommand "put --help"
+  -- runCommand "put 31 1000 2100 --note 21 --verbose"
+  -- runCommand "delete 31"
 
-  -- runCommand ["focus", "current"]
-  -- runCommand ["focus", "prev"]
-  -- runCommand ["focus", "none"]
-  -- runCommand ["focus"]
-  runCommand ["list", "--verbose"]
+  -- runCommand "focus current"
+  -- runCommand "focus prev"
+  -- runCommand "focus none"
+  -- runCommand "focus"
+  runCommand "list --verbose"
+
+  -- runCommand "hist"
 
   pure ()
 
-runCommand :: [Text] -> IO ()
+runCommand :: Text -> IO ()
 runCommand args = do
-  say $ "$ daimust " <> unwords args
-  withArgs (unpack <$> args) Cli.run
+  say $ "$ daimust " <> args
+  withArgs (unpack <$> words args) Cli.run
 
 invalidateCache :: IO ()
 invalidateCache =
